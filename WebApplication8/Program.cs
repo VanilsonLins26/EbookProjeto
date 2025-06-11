f6c70440b7d7203400a91becbd6277c817bbacf4
vanilson <vanilson.liins@gmail.com>
2025-06-11 14:08:43 -0300

    swagger

--- WebApplication8/Program.cs
+++ WebApplication8/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
@@ -12,6 +16,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "PDV PRODUTOS v1"));
 }
 
 app.UseHttpsRedirection();
