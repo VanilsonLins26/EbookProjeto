085a413c0e821f3fe03c962ea158146c9a91417d
vanilson <vanilson.liins@gmail.com>
2025-06-11 14:37:59 -0300

    bug2

--- WebApplication8/Program.cs
+++ WebApplication8/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:8080");
-
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -15,13 +13,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "PDV PRODUTOS v1"));
-}
 
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
